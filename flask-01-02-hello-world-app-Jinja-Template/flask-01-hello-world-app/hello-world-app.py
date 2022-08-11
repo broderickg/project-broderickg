bfc4e493d059136f89dbb8c7fadb4e6cdf8f2cb1
broderickg <broderick.gager8@gmail.com>
2022-08-10 19:11:01 -0700

    This is my first commit

---
+++ flask-01-02-hello-world-app-Jinja-Template/flask-01-hello-world-app/hello-world-app.py
@@ -0,0 +1,24 @@
+from flask import Flask 
+
+app = Flask(__name__)
+
+@app.route("/")
+def head():
+    return "<h1>You will be a good DevOps!</h1>"
+
+
+@app.route("/second")
+def second():
+    return "This is my second page"
+
+@app.route("/third/subthird")
+def third():
+    return "<h2>This is the subpath of third page. Broderick</h2>"
+
+@app.route("/forth/<string:id>")
+def forth(id):
+    return f'Id of this page is {id}'
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
